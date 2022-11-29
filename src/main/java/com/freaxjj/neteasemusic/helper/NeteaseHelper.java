fadd12ce6d4ed5ec6bb68fd5d1a6c15f07c2c7b8
freaxjj <1176648843@qq.com>
2022-11-29 22:06:02 +0800

    update

--- src/main/java/com/freaxjj/neteasemusic/helper/NeteaseHelper.java
+++ src/main/java/com/freaxjj/neteasemusic/helper/NeteaseHelper.java
@@ -142,7 +142,7 @@ public class NeteaseHelper {
     private <T> T doRequest(HttpMethod httpMethod, String url, Class<T> tClass, Map<String, Object> params) throws Exception {
         //加时间戳，防止netease api缓存
         params.put("timestamp", System.currentTimeMillis());
-        params.put("realIP", "138.2.45.55");
+//        params.put("realIP", "138.2.45.55");
         HttpEntity<Map<String, Object>> request;
         //跨域需要设置cookie
         if (!Consts.URL_LOGIN.equals(url)) {
