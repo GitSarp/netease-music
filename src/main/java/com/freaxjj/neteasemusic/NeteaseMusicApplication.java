44c236ee73908d37efc733855cad3fa9fdf885f4
GitSarp <1176648843@qq.com>
2023-01-18 05:42:21 +0000

    update

--- src/main/java/com/freaxjj/neteasemusic/NeteaseMusicApplication.java
+++ src/main/java/com/freaxjj/neteasemusic/NeteaseMusicApplication.java
@@ -2,9 +2,14 @@ package com.freaxjj.neteasemusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@RefreshScope
+//启用nacos服务发现
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NeteaseMusicApplication {
 
